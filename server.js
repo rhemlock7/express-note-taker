2f00d45826d225049fb56ff5b1fb5589d47ee868
Ryan Hemlock <ryan@ryanhemlock.com>
2023-12-21 12:03:21 -0500

    Created server.js file. Wrote initial code

---
+++ server.js
@@ -0,0 +1,22 @@
+// Importing express
+const express = require('express');
+// Importing the json file located in /db
+const db = require('./db/db.json');
+
+// Initialize Express
+const app = express();
+const PORT = 3001;
+
+app.get('/', (req, res) => {
+    res.send(
+        'Use the API endpoint at link'
+    )
+})
+
+// Get the data from the db.json file
+app.get('/db', (req, res) => res.json(db))
+
+// Have the server listening on port 3001
+app.listen(PORT, () =>
+    console.log(`Listening on: http://localhost:${PORT} !`)
+);
